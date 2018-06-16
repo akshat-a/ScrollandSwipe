e5854b43ac692e29369c208b5db8f839611e617d
Akshat Aggarwal <akshat010598@gmail.com>
2018-06-16 22:23:14 +0530

    Initial Commit

---
+++ swipe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swipe
+//
+//  Created by Akshat Aggarwal on 16/06/18.
+//  Copyright © 2018 Akshat Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
